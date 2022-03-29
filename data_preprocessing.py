b3be6dde6fd3001f0efaf26f2e631e2d9dd0661c
ai21m027 <ai21m027@technikum-wien.at>
2022-03-29 16:33:39 +0200

    Implemented multiple classes and multiple models

--- data_preprocessing.py
+++ data_preprocessing.py
@@ -1,6 +1,7 @@
 import json
 import re
 import warnings
+import random
 
 import gensim.models
 import matplotlib.pyplot as plt
@@ -13,6 +14,8 @@ from sklearn.ensemble import RandomForestClassifier
 from sklearn.manifold import TSNE
 from sklearn.metrics import precision_score, recall_score
 from sklearn.model_selection import train_test_split
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.neural_network import MLPClassifier
 
 warnings.filterwarnings(action='ignore')
 
@@ -55,6 +58,85 @@ def papers_by_mixed_categories(category: str, papers: list) -> list:
     return papers_wantedonly
 
 
+def tsne_plot(w2v_model,show_words=False):
+    plottable_data = [(i, np.array(w2v_model.wv[i])) for i in w2v_model.wv.index_to_key]
+    # plottable_data.columns = ['word','word-vector']
+
+    # test = plottable_data.loc[:,'word-vector']
+
+    tsne = TSNE(n_components=2, verbose=0, perplexity=40, n_iter=300)
+    tsne_results = tsne.fit_transform([x[1] for x in plottable_data])
+
+    tsne_df = pd.DataFrame()
+    tsne_df['tsne0'] = tsne_results[:, 0]
+    tsne_df['tsne1'] = tsne_results[:, 1]
+
+    plt.figure()
+    p1 = sns.scatterplot(x='tsne0', y='tsne1', palette=sns.color_palette('hls', 10), data=tsne_df, legend='full',
+                         alpha=0.3)
+    if show_words:
+        for i in range(len(plottable_data)):
+            p1.text(tsne_df.loc[i, 'tsne0'] + 0.01, tsne_df.loc[i, 'tsne1'],
+                    plottable_data[i][0], horizontalalignment='left',
+                    size='medium', color='black', weight='semibold')
+    plt.show()
+
+def pca_plot(w2v_model,show_words=False):
+    plottable_data = [(i, np.array(w2v_model.wv[i])) for i in w2v_model.wv.index_to_key]
+    pca = PCA(n_components=2)
+    pca_result = pca.fit_transform([x[1] for x in plottable_data])
+    pca_result = pca_result
+
+    pca_df = pd.DataFrame()
+    pca_df['pca0'] = pca_result[:, 0]
+    pca_df['pca1'] = pca_result[:, 1]
+
+    plt.figure()
+    p1 = sns.scatterplot(x='pca0', y='pca1', palette=sns.color_palette('hls', 10), data=pca_df, legend='full',
+                         alpha=0.3)
+    if show_words:
+        for i in range(len(plottable_data)):
+            p1.text(pca_df.loc[i, 'pca0'] + 0.01, pca_df.loc[i, 'pca1'],
+                    plottable_data[i][0], horizontalalignment='left',
+                    size='medium', color='black', weight='semibold')
+    plt.show()
+
+    pca = PCA(n_components=3)
+    pca_result = pca.fit_transform([x[1] for x in plottable_data])
+    pca_result = pca_result
+
+    pca_df = pd.DataFrame()
+    pca_df['pca0'] = pca_result[:, 0]
+    pca_df['pca1'] = pca_result[:, 1]
+    pca_df['pca2'] = pca_result[:, 2]
+    ax = plt.figure().gca(projection='3d')
+    ax.scatter(xs=pca_df['pca0'],
+               ys=pca_df['pca1'],
+               zs=pca_df['pca2'],
+               cmap='tab10')
+    plt.show()
+
+
+def class_balancer(dataframes:list,size:int)->pd.DataFrame:
+    """
+    Balances classes by random selection with replacement
+    :param dataframes: list of pd.Dataframes each one representing 1 class
+    :param size: size of output pd.Dataframe
+    :return: shuffled pd.Dataframe with equal class representation
+    """
+    classes = len(dataframes)
+    output_data = pd.DataFrame()
+    for i in range(size):
+        cat = random.randint(0, classes-1)
+        el = random.randint(0,len(dataframes[cat].index)-1)
+        test = dataframes[cat].iloc[el,:]
+        output_data = pd.concat([output_data,pd.DataFrame(dataframes[cat].iloc[el, :]).T])
+    output_data.reset_index(drop=True,inplace=True)
+    return output_data
+
+
+
+
 def main() -> None:
     pd.set_option('display.max_columns', None)
     file = r'arxiv-metadata-oai-snapshot.json'
@@ -78,22 +160,42 @@ def main() -> None:
     # print(len(papers_mathonly))
     papers_csonly = papers_by_single_category('^cs', data)
     # print(len(papers_csonly))
-    """
+    papers_econonly = papers_by_single_category('econ', data)
+    #print(len(papers_econonly))
+    # 0 econ only papers
+    papers_nlinonly = papers_by_single_category('nlin',data)
+    #print(len(papers_nlinonly))
+    papers_qbioonly = papers_by_single_category('q-bio',data)
+    #print(len(papers_qbioonly))
+
     categories = set([el['categories'] for el in data])
+    categories = sorted(categories)
     for el in categories:
-        print(el)
-    """
+        #print(el)
+        pass
+
 
     data_physonly = pd.DataFrame([x['abstract'] for x in papers_physonly])
     data_physonly['class'] = (['physics' for x in range(len(data_physonly))])
     data_mathonly = pd.DataFrame([x['abstract'] for x in papers_mathonly])
     data_mathonly['class'] = (['math' for x in range(len(papers_mathonly))])
+    data_nlinonly = pd.DataFrame([x['abstract'] for x in papers_nlinonly])
+    data_nlinonly['class'] = (['nlin' for x in range(len(papers_nlinonly))])
     data_csonly = pd.DataFrame([x['abstract'] for x in papers_csonly])
     data_csonly['class'] = (['cs' for x in range(len(papers_csonly))])
+    data_qbioonly = pd.DataFrame([x['abstract'] for x in papers_qbioonly])
+    data_qbioonly['class'] = (['qbio' for x in range(len(papers_qbioonly))])
 
     data = data_physonly.append(data_mathonly)
-    # data = data.append(data_csonly)
+    data = data.append(data_csonly)
+    data = data.append(data_csonly)
+    data = data.append(data_csonly)
+    data.reset_index(drop=True,inplace=True)
 
+    # Data balanced for equal class representation
+    equal_data = class_balancer([data_physonly,data_mathonly,data_csonly,data_nlinonly,data_qbioonly],10000)
+    equal_data.columns= ['abstract','class']
+    data=equal_data
     tokenized_abstracts = []
     for index, abstract in enumerate(data.iloc[:, 0]):
         abs = str(abstract.replace("\n", " "))
@@ -111,69 +213,19 @@ def main() -> None:
     data['clean'] = tokenized_abstracts
     data.columns = ['text', 'class', 'clean']
 
-    data['class'] = data['class'].map({'physics': 0, 'math': 1})
-    for i in range(1):
+    data['class'] = data['class'].map({'physics': 0, 'math': 1,'cs':2,'nlin':3,'qbio':4})
+    #print(data.describe())
+    for i in range(10):
         X_train, X_test, Y_train, Y_test = train_test_split(data['clean'], data['class'], test_size=0.2)
 
         w2v_model = gensim.models.Word2Vec(X_train,
                                            vector_size=100,
                                            window=5,
                                            min_count=2)
-
         show_words = False
-        plottable_data = [(i, np.array(w2v_model.wv[i])) for i in w2v_model.wv.index_to_key]
-        # plottable_data.columns = ['word','word-vector']
 
-        # test = plottable_data.loc[:,'word-vector']
-
-        tsne = TSNE(n_components=2, verbose=1, perplexity=40, n_iter=300)
-        tsne_results = tsne.fit_transform([x[1] for x in plottable_data])
-
-        tsne_df = pd.DataFrame()
-        tsne_df['tsne0'] = tsne_results[:, 0]
-        tsne_df['tsne1'] = tsne_results[:, 1]
-
-        plt.figure()
-        sns.scatterplot(x='tsne0', y='tsne1', palette=sns.color_palette('hls', 10), data=tsne_df, legend='full',
-                        alpha=0.3)
-        if show_words:
-            for i in range(len(plottable_data)):
-                p1.text(tsne_df.loc[i, 'tsne0'] + 0.01, tsne_df.loc[i, 'tsne1'],
-                        plottable_data[i][0], horizontalalignment='left',
-                        size='medium', color='black', weight='semibold')
-
-        pca = PCA(n_components=2)
-        pca_result = pca.fit_transform([x[1] for x in plottable_data])
-        pca_result = pca_result
-
-        pca_df = pd.DataFrame()
-        pca_df['pca0'] = pca_result[:, 0]
-        pca_df['pca1'] = pca_result[:, 1]
-
-        plt.figure()
-        p1 = sns.scatterplot(x='pca0', y='pca1', palette=sns.color_palette('hls', 10), data=pca_df, legend='full',
-                             alpha=0.3)
-        if show_words:
-            for i in range(len(plottable_data)):
-                p1.text(pca_df.loc[i, 'pca0'] + 0.01, pca_df.loc[i, 'pca1'],
-                        plottable_data[i][0], horizontalalignment='left',
-                        size='medium', color='black', weight='semibold')
-        plt.show()
-
-        pca = PCA(n_components=3)
-        pca_result = pca.fit_transform([x[1] for x in plottable_data])
-        pca_result = pca_result
-
-        pca_df = pd.DataFrame()
-        pca_df['pca0'] = pca_result[:, 0]
-        pca_df['pca1'] = pca_result[:, 1]
-        pca_df['pca2'] = pca_result[:, 2]
-        ax = plt.figure().gca(projection='3d')
-        ax.scatter(xs=pca_df['pca0'],
-                   ys=pca_df['pca1'],
-                   zs=pca_df['pca2'],
-                   cmap='tab10')
-        plt.show()
+        #tsne_plot(w2v_model,show_words)
+        #pca_plot(w2v_model,show_words)
 
         # print(w2v_model.wv.most_similar('paper'))
 
@@ -202,12 +254,35 @@ def main() -> None:
 
         y_pred = rf_model.predict(X_test_vect_avg)
 
-        precision = precision_score(Y_test, y_pred)
-        recall = recall_score(Y_test, y_pred)
-        print(f'Precision: {round(precision, 3)} /'
+        precision = precision_score(Y_test, y_pred,average='macro')
+        recall = recall_score(Y_test, y_pred,average='macro')
+        print(f'Random forest: Precision: {round(precision, 3)} /'
               f' Recall: {round(recall, 3)} /'
               f' Accuracy: {round((y_pred == Y_test).sum() / len(y_pred), 3)}')
 
+        kn=KNeighborsClassifier()
+        kn_model = kn.fit(X_train_vect_avg, Y_train.values.ravel())
+
+        y_pred = kn_model.predict(X_test_vect_avg)
+
+        precision = precision_score(Y_test, y_pred,average='macro')
+        recall = recall_score(Y_test, y_pred,average='macro')
+        print(f'KNN: Precision: {round(precision, 3)} /'
+              f' Recall: {round(recall, 3)} /'
+              f' Accuracy: {round((y_pred == Y_test).sum() / len(y_pred), 3)}')
+
+
+        nn=MLPClassifier(verbose=False)
+        nn_model = nn.fit(X_train_vect_avg, Y_train.values.ravel())
+
+        y_pred = nn_model.predict(X_test_vect_avg)
+
+        precision = precision_score(Y_test, y_pred,average='macro')
+        recall = recall_score(Y_test, y_pred,average='macro')
+        print(f'Neural Net: Precision: {round(precision, 3)} /'
+              f' Recall: {round(recall, 3)} /'
+              f' Accuracy: {round((y_pred == Y_test).sum() / len(y_pred), 3)}')
+        print('\n')
 
 if __name__ == '__main__':
     main()
